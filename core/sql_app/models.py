1c62fe068e690ad4b0cedad457cf4853529a36b1
PierreKimbanziR <kimbanzip@gmail.com>
2021-10-15 13:53:59 +0200

    add models

---
+++ core/sql_app/models.py
@@ -0,0 +1,25 @@
+from .database import Base
+
+from sqlalchemy import Column, ForeignKey, Integer, Float, String, Date
+from sqlalchemy.orm import relationship
+
+class Team(Base):
+    __tablename__ = "teams"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255), unique=True)
+    city = Column(String(255))
+    players = relationship("Player", back_populates="team")
+
+
+class Player(Base):
+    __tablename__ ="players"
+    id = Column(Integer, primary_key=True, index=True)
+    first_name = Column(String(255))
+    last_name = Column(String(255), index=True)
+    birth_date = Column(Date)
+    weight = Column(Float)
+    height = Column(Float)
+    position = Column(String(255))
+    team_name = Column(String(255), ForeignKey("teams.name"))
+
+    team = relationship("Team", back_populates="players")
